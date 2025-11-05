2b4b7c22ca696ba42651d1ea3ab6eb5a28d3b94b
Ranjeet <ranjitkumar123bind@gmail.com>
2025-11-05 08:22:47 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import random
 
-print("🎯 Welcome to Number Guessing Game 🎯")
+print(" Welcome to Number Guessing Game ")
 print("I'm thinking of a number between 1 and 100...")
 
 # Random number generate
@@ -8,18 +8,18 @@ secret = random.randint(1, 100)
 attempts = 0
 
 while True:
-    guess = input("👉 Enter your guess: ")
+    guess = input(" Enter your guess: ")
     if not guess.isdigit():
-        print("⚠️ Please enter a valid number!")
+        print(" Please enter a valid number!")
         continue
 
     guess = int(guess)
     attempts += 1
 
     if guess < secret:
-        print("🔻 Too low! Try again.")
+        print(" Too low! Try again.")
     elif guess > secret:
-        print("🔺 Too high! Try again.")
+        print(" Too high! Try again.")
     else:
-        print(f"🎉 Congratulations! You guessed it right in {attempts} attempts!")
+        print(f" Congratulations! You guessed it right in {attempts} attempts!")
         break
